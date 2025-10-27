fdf66c6b8f26ffe9ef9eb16684e85796d4a34745
besnn <besniknuro74@gmail.com>
2025-10-27 20:21:45 +0100

    add basic runnable main

--- src/main/java/me/besnn/CLI.java
+++ src/main/java/me/besnn/CLI.java
@@ -1,6 +1,17 @@
 package me.besnn;
 
-public class CLI {
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "idf", mixinStandardHelpOptions = true,
+        version = "0.0.1",
+        description = "for now asdf")
+public class CLI implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
 }
 
 //TODO: implement
