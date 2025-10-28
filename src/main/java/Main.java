1371c96b72ddd005c077e8797be941f199bdfd37
besnn <besniknuro74@gmail.com>
2025-10-28 23:42:28 +0100

    delete Exif Debug classes PHashDct in working order

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,14 @@
 import app.CLI;
 
-import core.ExifDebug;
-import core.ExifDebugFixed;
 import picocli.CommandLine;
-
-import app.Commands;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String... args) throws Exception {
-        ExifDebugFixed.main(args);
-//        System.out.println(Arrays.toString(args));
-//        int exitCode = new CommandLine(new CLI()).execute(args);
-//        System.exit(exitCode);
+//        ExifDebugFixed.main(args);
+        System.out.println(Arrays.toString(args));
+        int exitCode = new CommandLine(new CLI()).execute(args);
+        System.exit(exitCode);
     }
 }
